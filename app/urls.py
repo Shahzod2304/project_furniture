eb4c894cabee1be275e7e167af6a18183d8fc6e2
Shahzod_Sharofiddinov <sharofiddinovshahzod3@gmail.com>
2024-02-05 19:49:57 +0500

    update

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from .views import * 
+
+
+urlpatterns = [
+    path('', Home, name='home'),
+    path('about/', About, name='about'),
+    path('blog/', Blog, name='blog'),
+    path('contact/', Contact, name='contact'),
+    path('furniture/', Furniture, name='furniture'),
+]
